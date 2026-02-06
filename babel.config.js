d0900184617a73d5c83ec58a43a8953b42f1778e
Dev-HNY <hnyadav6271@gmail.com>
2026-02-06 14:06:11 +0530

    Ready for production build 1

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
+
+  const plugins = ['react-native-reanimated/plugin'];
+
+  // Remove all console statements in production builds
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(['transform-remove-console', { exclude: [] }]);
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins,
   };
 };
